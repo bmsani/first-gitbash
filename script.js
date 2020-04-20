625a1ea36059aecd5188d127f97e78b4a97e1b27
bmsani <bmsani6@gmail.com>
2020-04-20 21:54:56 +0600

    This is new commit

---
+++ script.js
@@ -0,0 +1,34 @@
+// function sum(...rest){
+//     return rest.reduce((a,b) => a + b)
+// }
+
+// console.log(sum(1,2,3,4,5,6))
+
+let a = [1, 2, 3, 4, 5, 6]
+
+console.log(...a)
+
+let obj = {
+    a: 10,
+    b: 20,
+    c:30
+}
+
+let obj2 = {
+    ...obj
+}
+console.log(obj2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
